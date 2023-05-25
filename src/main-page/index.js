b6b90f27ae949aaecaa9b5a51b0218f8530401cb
PavanaKatta <pavanakatta@gmail.com>
2023-05-25 16:26:08 +0530

    Initial commit

---
+++ src/main-page/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../style.css';
+import './main-page.css';
+
+let src = 'Src1';
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="red">Hello StackBlitz! {src}</h1>
+      <p className="p-color">Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
